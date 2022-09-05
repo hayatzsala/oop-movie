911e917288ade39e1e4a9d677ff0d01d2bd9f8f3
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-09-05 17:41:22 +0300

    Fix the design

--- oop-script.js
+++ oop-script.js
@@ -30,24 +30,30 @@ class HomePage {
     static container = document.getElementById('container');
     static renderMovies(movies) {
         const row = document.createElement('div');
-        row.className="row row-cols-3";
+        row.className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1";
         movies.forEach(movie => {
             const movieCard = document.createElement("div");
-            movieCard.className="p-3 col card";
+            movieCard.className="col card";
             const movieDiv = document.createElement("div");
             movieDiv.className="card-body";
             const movieImage = document.createElement("img");
-            movieImage.className = "card-img-top";
+           // movieImage.className = "card-img-top";
             movieImage.src = `${movie.backdropUrl}`;
             const movieTitle = document.createElement("h3");
             movieTitle.className = "card-title"; 
             movieTitle.textContent = `${movie.title}`;
+
+            const rate = document.createElement('div');
+            rate.innerHTML=`<span>Rate : ${movie.rate}</span>`;
+
             movieImage.addEventListener("click", function() {
                 Movies.run(movie);
             });
 
             movieDiv.appendChild(movieTitle);
+            movieDiv.appendChild(rate);
             movieDiv.appendChild(movieImage);
+
             movieCard.appendChild(movieDiv);
             row.appendChild(movieCard);
             this.container.appendChild(row);
@@ -102,6 +108,10 @@ class Movie {
         this.runtime = json.runtime + " minutes";
         this.overview = json.overview;
         this.backdropPath = json.backdrop_path;
+        this.rate = json.vote_average;
+        this.genres = json.genres;
+        console.log(this.genres)
+        console.log(json);
     }
 
     get backdropUrl() {
